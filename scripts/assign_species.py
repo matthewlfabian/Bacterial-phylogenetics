ed22daa1c3b716f96b7260ebe75abfb0fe197768
matthewlfabian <116763086+matthewlfabian@users.noreply.github.com>
2026-04-13 15:38:23 -0400

    Update assign_species.py

--- scripts/assign_species.py
+++ scripts/assign_species.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import networkx as nx
 
-ani = pd.read_csv(snakemake.input.ani, sep="\t" header=None,
+ani = pd.read_csv(snakemake.input.ani, sep="\t", header=None,
                   names=["strain_1", "strain_2", "ANI", "frag_mapped", "frag_total"])
 meta = pd.read_csv(snakemake.input.meta, sep="\t")
 
